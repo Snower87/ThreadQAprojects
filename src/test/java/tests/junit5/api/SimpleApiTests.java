1e29f063e9121d761d4de65689c7713eb8a96886
Беглецов Сергей Николаевич <snower87@yandex.ru>
2025-06-21 13:46:49 +0300

    44.  Отправка GET запроса и сохранение в переменную, сравнение двух списков, проверка на сортировку

--- src/test/java/tests/junit5/api/SimpleApiTests.java
+++ src/test/java/tests/junit5/api/SimpleApiTests.java
@@ -1,11 +1,17 @@
 package tests.junit5.api;
 
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.Matchers.*;
 
 public class SimpleApiTests {
     String base_url = "https://fakestoreapi.in/api/users/";
@@ -45,5 +51,59 @@ public class SimpleApiTests {
                 .body("user.id", equalTo(5))
                 .body("user.address.city", equalTo("West Paulport"))
                 .log().all();
+    }
+
+    @Test
+    public void getAllUsersWithLimitTest() {
+        //1. высылаем запрос с queryParam: api/users?limit=2
+        int limitSize = 2;
+        given().queryParam("limit", limitSize)
+                .when()
+                .get(base_url)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("users", Matchers.hasSize(limitSize));
+    }
+
+    @Test
+    public void getAllUsersSortByDescTest() {
+        //1. тестируем сортировку данных, полученных с сайта и собственную
+        //2. высылаем запрос с участие queryParam: /api/users?sort=desc
+        String sortType = "desc";
+        Response sortedResponse = given().queryParam("sort", sortType)
+                .get("https://fakestoreapi.in/api/users/")
+                .then().log().all()
+                .statusCode(200)
+                .extract().response();
+
+        Response notSorted = given()
+                .get("https://fakestoreapi.in/api/users/")
+                .then().log().all()
+                .extract().response();
+
+        List<Integer> sortedResponseIds = sortedResponse.jsonPath().getList("users.id");
+        List<Integer> notSortedIds = notSorted.jsonPath().getList("users.id");
+
+        List<Integer> sortedByCode = notSortedIds.stream()
+                .sorted(Comparator.reverseOrder())    // reverseOrder() - инверстно (от большего к меньшего)
+                                                      // naturalOrder () - естественный порядок (по возрастанию)
+                .toList();      // -> .collect(Collectors.toList());
+
+        //Actual   :[20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        Assertions.assertNotEquals(sortedResponseIds, notSortedIds);
+        Assertions.assertEquals(sortedByCode, sortedResponseIds);
+    }
+
+    @Test
+    public void getTimeResponse() {
+        //1. получаем время ответа от сервера
+        long time = given()
+                .get(base_url)
+                .then()
+                .statusCode(200)
+                .extract()
+                .timeIn(TimeUnit.MILLISECONDS);
+        System.out.println("Time responce: " + time + " ms");
     }
 }
