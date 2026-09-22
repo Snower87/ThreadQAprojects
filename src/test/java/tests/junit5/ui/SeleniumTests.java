8365d87cc3aafb48f5ec7132d3f34b0d91992af5
Беглецов Сергей Николаевич <snower87@yandex.ru>
2026-09-22 08:39:37 +0300

    58. Работа с WebElement, поиск по Xpath, строение теста

--- src/test/java/tests/junit5/ui/SeleniumTests.java
+++ src/test/java/tests/junit5/ui/SeleniumTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -24,13 +26,59 @@ public class SeleniumTests {
     }
 
     @Test
-    public void simpleTest() {
+    public void simpleUITest() {
         String expectedTitle = "Oleg Pendrak | ThreadQA";
 
         driver.get("https://threadqa.ru");
         String actualTitle = driver.getTitle();
 
         Assertions.assertEquals(expectedTitle, actualTitle);
+    }
+
+    @Test
+    public void simpleForTest() {
+        //1. Тестовые данные в самом верху
+        String expectedName = "Tomas Anderson";
+        String expectedEmail = "tomas@matrix.ru";
+        String expectedCurrentAddress = "USA Los Angeles";
+        String expectedPermanentAddress = "USA Miami";
+
+        //2. Какие-то операции: заходим, кликаем
+        driver.get("http://85.192.34.140:8081");
+        WebElement elementsCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Elements']"));
+        elementsCard.click();
+        WebElement elementsTextBox = driver.findElement(By.xpath("//span[text()='Test Box']"));
+        elementsTextBox.click();
+
+        //3. Ищем все элементы на странице
+        WebElement fullName = driver.findElement(By.id("userName"));
+        WebElement email = driver.findElement(By.id("userEmail"));
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        WebElement submit = driver.findElement(By.id("submit"));
+
+        fullName.sendKeys(expectedName);
+        email.sendKeys(expectedEmail);
+        currentAddress.sendKeys(expectedCurrentAddress);
+        permanentAddress.sendKeys(expectedPermanentAddress);
+        submit.click();
+
+        //4. Получаем свежие элементы после взаимодействия
+        WebElement nameNew = driver.findElement(By.id("name"));
+        WebElement emailNew = driver.findElement(By.id("email"));
+        WebElement currentAddressNew = driver.findElement(By.xpath("//div[@id='output']/p[@id='currentAddress']"));
+        WebElement permanentAddressNew = driver.findElement(By.xpath("//div[@id='output']/p[@id='permanentAddress']"));
+
+        //5. Матчим, сравниваем ожидаемый и реальный результат
+        String actualName = nameNew.getText();
+        String actualEmail = emailNew.getText();
+        String actualCurrentAddress = currentAddressNew.getText();
+        String actualPermanentAddress = permanentAddressNew.getText();
+
+        Assertions.assertTrue(actualName.contains(expectedName));
+        Assertions.assertTrue(actualEmail.contains(expectedEmail));
+        Assertions.assertTrue(actualCurrentAddress.contains(expectedCurrentAddress));
+        Assertions.assertTrue(actualPermanentAddress.contains(expectedPermanentAddress));
     }
 
     @AfterEach
